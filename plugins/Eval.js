ea4b7504a24e5a4471ad3ed687bd93027d5d15f8
⚡ CalciFer <168836504+CalciFer-Howl@users.noreply.github.com>
2024-09-26 02:17:13 +0530

    Update Eval.js

--- plugins/Eval.js
+++ plugins/Eval.js
@@ -1,24 +1,24 @@
-import syntaxerror from 'syntax-error'
-import { format } from 'util'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import { createRequire } from 'module'
+import syntaxerror from 'syntax-error';
+import { format } from 'util';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createRequire } from 'module';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
-const baileys = require('@whiskeysockets/baileys')
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const baileys = require('@whiskeysockets/baileys');
 
 let handler = async (m, _2) => {
-  let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
-  let _return
-  let _syntax = ''
-  let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
-  let old = m.exp * 1
+  let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2;
+  let _return;
+  let _syntax = '';
+  let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix;
+  let old = m.exp * 1;
   try {
-    let i = 15
+    let i = 15;
     let f = {
       exports: {},
-    }
+    };
     let exec = new (async () => {}).constructor(
       'print',
       'm',
@@ -34,13 +34,13 @@ let handler = async (m, _2) => {
       'exports',
       'argument',
       _text
-    )
+    );
     _return = await exec.call(
       conn,
       (...args) => {
-        if (--i < 1) return
-        console.log(...args)
-        return conn.reply(m.chat, format(...args), m)
+        if (--i < 1) return;
+        console.log(...args);
+        return conn.reply(m.chat, format(...args), m);
       },
       m,
       handler,
@@ -54,29 +54,32 @@ let handler = async (m, _2) => {
       f,
       f.exports,
       [conn, _2]
-    )
+    );
   } catch (e) {
     let err = syntaxerror(_text, 'Execution Function', {
       allowReturnOutsideFunction: true,
       allowAwaitOutsideFunction: true,
       sourceType: 'module',
-    })
-    if (err) _syntax = '```' + err + '```\n\n'
-    _return = e
+    });
+    if (err) _syntax = '```' + err + '```\n\n';
+    _return = e;
   } finally {
-    conn.reply(m.chat, _syntax + format(_return), m)
-    m.exp = old
+    conn.reply(m.chat, _syntax + format(_return), m);
+    m.exp = old;
   }
-}
-handler.customPrefix = /^=?> /
-handler.command = new RegExp()
-handler.rowner = true
+};
 
-export default handler
+// Modify the customPrefix to detect `>` immediately followed by a command without space
+handler.customPrefix = /^>?/;
+handler.command = new RegExp();
+handler.developer = true;
+
+export default handler;
 
 class CustomArray extends Array {
   constructor(...args) {
-    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000))
-    else return super(...args)
+    if (typeof args[0] == 'number') return super(Math.min(args[0], 10000));
+    else return super(...args);
   }
-}
+  }
+  
