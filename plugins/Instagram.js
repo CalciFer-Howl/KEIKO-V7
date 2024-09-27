b6cdbb837476fe0d7cb6b223022eaf6610dc7b72
⚡ CalciFer <168836504+CalciFer-Howl@users.noreply.github.com>
2024-09-27 15:49:25 +0530

    Installed 🤍✨

---
+++ plugins/Instagram.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command, text }) => {
+m.react('⏳')
+if (!text) throw `_Give Me A Link For Download 🌸_`
+  const igdl = `${calcifer}insta/v7/igdl?url=`
+let fet = igdl + text
+ let res = await fetch(fet);
+const data = await res.json();
+const open = data.downloadLinks
+ for (const Meera of open) {
+const mimmi = Meera.url
+const mediaType = Meera.type
+m.reply(mediaType)
+let Cap = '*-Take Your Video🧚🏻‍♂️⚡*'
+let CapI = '*-Take Your Image🧚🏻‍♂️🤍*'
+
+
+conn.sendFile(m.chat, mimmi, 'keiko.mp4', Cap, m)
+// conn.sendFile(m.chat, mimmi, 'keiko.jpg', CapI, m)
+
+}
+
+m.react('✅')
+}
+
+handler.help = ['download']
+handler.tags = ['internet']
+handler.command = /^(insta|ig|instagram)$/i
+export default handler
